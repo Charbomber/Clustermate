6843ba9df303eed5056129ee6fbc2b2d7176eab0
Charbomber <33207044+Charbomber@users.noreply.github.com>
2023-10-02 14:12:33 -0400

    fixes yippeee

--- data/start.lua
+++ data/start.lua
@@ -1,24 +1,21 @@
 
+local newButton = loveframes.Create("button")
 
-local startGUI = {}
-
-startGUI[1] = loveframes.Create("button")
-
-startGUI[1]:SetState("start")
-startGUI[1]:SetWidth(256)
-startGUI[1]:SetPos(32, 128, false)
-startGUI[1]:SetText("New Cluster")
-startGUI[1].OnClick = function(obj, x, y)
+newButton:SetState("start")
+newButton:SetWidth(256)
+newButton:SetPos(32, 128, false)
+newButton:SetText("New Cluster")
+newButton.OnClick = function(obj, x, y)
     obj:SetText("Doesn't work yet lol")
 end
 
 
-startGUI[2] = loveframes.Create("button")
+local loadButton = loveframes.Create("button")
 
-startGUI[2]:SetState("start")
-startGUI[2]:SetWidth(256)
-startGUI[2]:SetPos(32, 128+32, false)
-startGUI[2]:SetText("Load Cluster")
-startGUI[2].OnClick = function(obj, x, y)
+loadButton:SetState("start")
+loadButton:SetWidth(256)
+loadButton:SetPos(32, 128+32, false)
+loadButton:SetText("Load Cluster")
+loadButton.OnClick = function(obj, x, y)
     obj:SetText("Doesn't work yet lol")
 end
